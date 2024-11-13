65c9fafca46930c1998e2079143ca6c91dd0d688
MrHasak99 <hamad_alkhalaf_99@hotmail.com>
2024-11-13 20:45:57 +0300

    task complete

--- src/Components/PetItem.js
+++ src/Components/PetItem.js
@@ -1,14 +1,30 @@
-function PetItem({ pet }) {
+import { useState } from "react";
+
+function PetItem({ pet, adopt }) {
+  const [petImage, setPetImage] = useState(pet.image);
+
+  const changeImage = () => {
+    if (petImage === pet.image) {
+      setPetImage(pet.image2);
+    } else {
+      setPetImage(pet.image);
+    }
+  };
+
   return (
     <div className="col-lg-4 col-md-8 col-sm-10">
       <div className="single-doctor">
-        <img className="image" alt={pet.name} src={pet.image} />
+        <img className="image" alt={pet.name} src={petImage} />
         <div className="content">
           <h3>{pet.name}</h3>
-          <button type="button" className="btn btn-info">
+          <button type="button" className="btn btn-info" onClick={changeImage}>
             Pet
           </button>
-          <button type="button" class="btn btn-info  m-2">
+          <button
+            type="button"
+            class="btn btn-info  m-2"
+            onClick={() => adopt(pet.id)}
+          >
             Adopt
           </button>
         </div>
--- src/Components/PetsList.js
+++ src/Components/PetsList.js
@@ -1,8 +1,23 @@
+import { useState } from "react";
 import pets from "../petsData";
 import PetItem from "./PetItem";
 
 function PetsList() {
-  const petList = pets.map((pet) => <PetItem pet={pet} key={pet.id} />);
+  const [query, setQuery] = useState("");
+  const [available, setAvailable] = useState(pets);
+  const handleAdopt = (petId) => {
+    const petList = available.filter((pet) => pet.id !== petId);
+    setAvailable(petList);
+  };
+  const filterPets = available.filter((pet) =>
+    pet.name.toLowerCase().includes(query)
+  );
+  const [type, setType] = useState("");
+  const filterType = filterPets.filter((pet) => pet.type.includes(type));
+
+  const petList = filterType.map((pet) => (
+    <PetItem adopt={handleAdopt} pet={pet} key={pet.id} />
+  ));
 
   return (
     <section id="doctors" className="doctor-section pt-140">
@@ -20,11 +35,15 @@ function PetsList() {
                   placeholder="Search"
                   aria-label="Search"
                   aria-describedby="search-addon"
+                  onChange={(event) => setQuery(event.target.value)}
                 />
               </div>
               <br />
               Type:
-              <select className="form-select">
+              <select
+                className="form-select"
+                onChange={(event) => setType(event.target.value)}
+              >
                 <option value="" selected>
                   All
                 </option>
